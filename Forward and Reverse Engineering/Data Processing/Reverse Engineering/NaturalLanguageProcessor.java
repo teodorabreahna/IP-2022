c599951471cf296463b5a6d98d8c0643935411b9
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-03-21 09:33:38 +0200

    Reverse Engineering
    Co-Authored-By: AlexandraCrd <79160866+AlexandraCrd@users.noreply.github.com>

---
+++ Forward and Reverse Engineering/Data Processing/Reverse Engineering/NaturalLanguageProcessor.java
@@ -0,0 +1,19 @@
+// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   NaturalLanguageProcessor.java
+
+
+public class NaturalLanguageProcessor extends Processor
+{
+
+    public NaturalLanguageProcessor()
+    {
+    }
+
+    public void uses(Processor processor)
+    {
+    }
+
+    private String nlp_tool;
+}
