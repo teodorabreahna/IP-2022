e7d3134ccf4669b15846e4c7207770856553f7a8
alupoaiemircea <alupoaiemircea@gmail.com>
2022-03-21 08:30:21 +0200

    a

---
+++ argoUml_classes/WebInterface.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class WebInterface {
+
+  public String input;
+
+  public Integer newAttr;
+
+    public Vector  myWordMker;
+
+  public String getInput( String) {
+  return null;
+  }
+
+}
---
+++ argoUml_classes/WordMker.java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+
+public class WordMker {
+
+  public String relatedWords;
+
+      public Vector  mySpider;
+
+  public String getRelatedWords( String) {
+  return null;
+  }
+
+}
