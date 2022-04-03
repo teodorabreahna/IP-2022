7d5465c57196928c448ba9dfd190804f1b0cd542
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-04-03 16:50:00 +0300

    First try at data collection

--- FirstIteration/Crawler/src/main/java/tumblrcollect/Main.java
+++ FirstIteration/Crawler/src/main/java/tumblrcollect/Main.java
@@ -1,10 +1,13 @@
 package tumblrcollect;
 
 
+import com.tumblr.jumblr.JumblrClient;
 
 public class Main {
     public static void main(String[] args){
         Connect c = new Connect();
-        c.newClient();
+        Collect t = new Collect();
+        JumblrClient client = c.newClient();
+        //t.getTags(client,"gif"); - have to figure out why it throws the error
     }
 }
