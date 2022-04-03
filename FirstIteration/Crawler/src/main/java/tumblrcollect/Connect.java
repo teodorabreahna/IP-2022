10b497bb1e59e6a18afb9d3379bce8afa324f60d
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-04-03 16:26:03 +0300

    Tumblr part of the Crawler, mainly the connection process

---
+++ FirstIteration/Crawler/src/main/java/tumblrcollect/Connect.java
@@ -0,0 +1,31 @@
+package tumblrcollect;
+
+import com.tumblr.jumblr.JumblrClient;
+import com.tumblr.jumblr.types.Post;
+import com.tumblr.jumblr.types.User;
+
+import java.util.List;
+
+public class Connect {
+    public void newClient(){
+
+        /**
+         * The authentication to the API.
+         */
+
+        JumblrClient client = new JumblrClient(
+                "uvrLjpdP7LfXWHAoqTx45v3ENsqHFCltnrxrzOKYaNjNmDANXf",
+                "HwiVenpfbvcgaIm6SBqo8ZMEm72Ei2OpUXlaaVBs8yQEKijmXQ"
+        );
+        client.setToken(
+                "KVHjWRp4bK1wWEexCXGOo1PVbYceBqSySp8RBSlrf3uoU9G5se",
+                "yLVlgy3y0Cd7N56jCoB1RlOjCSnqDbAyR6bAPcRVTo04NCOUEL"
+        );
+        User user = client.user();
+        System.out.println(user.getName());
+
+        /*for(Post post : client.tagged("lol")){
+            post.getId();
+        }*/
+    }
+}
---
+++ FirstIteration/Crawler/src/main/java/tumblrcollect/Main.java
@@ -0,0 +1,10 @@
+package tumblrcollect;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        Connect c = new Connect();
+        c.newClient();
+    }
+}
