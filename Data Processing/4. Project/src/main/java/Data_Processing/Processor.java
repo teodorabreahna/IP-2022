5b502e5e25a3c856a3a315c002c8f5a963df4676
AlexandraCrd <alexandra.cardas1@gmail.com>
2022-04-04 08:49:37 +0300

    Update Processor.java

--- Data Processing/4. Project/src/main/java/Data_Processing/Processor.java
+++ Data Processing/4. Project/src/main/java/Data_Processing/Processor.java
@@ -1,10 +1,16 @@
+/*
+The "Processor" Class is responsible with processing all the information. It uses the class "Natural Language Processor"
+to call the NLP Tool to apply on our data and the "GetInput" class to get the external data from the Crawler team.
+We also need a file with the same name, where the class will be located.
+ */
+
 package Data_Processing;
 
 public class Processor {
 
+    int currentUtilization;
     private String name;
-    private String deviceID;
-    int CurrentUtilization;
+    private String deviceId;
 
     public Processor(String name) {
         this.name = name;
@@ -14,12 +20,12 @@ public class Processor {
         return name;
     }
 
-    public String getDeviceID() {
-        return deviceID;
+    public String getDeviceId() {
+        return deviceId;
     }
 
     public int getCurrentUtilization() {
-        return CurrentUtilization;
+        return currentUtilization;
     }
 
     public void getInfoFromCrawler(){
