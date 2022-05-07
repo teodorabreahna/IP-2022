d341e5eb6507494efa5d51bc353af048726470dd
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-05-07 12:20:51 +0300

    Create DemoDataProcessingApplication.java

---
+++ Data Processing/DemoDataProcessing/src/main/java/Demo/DemoDataProcessing/DemoDataProcessingApplication.java
@@ -0,0 +1,12 @@
+package Demo.DemoDataProcessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDataProcessingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDataProcessingApplication.class, args);
+	}
+
+}
