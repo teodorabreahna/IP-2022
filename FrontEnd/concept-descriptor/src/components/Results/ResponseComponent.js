a0da3cbe6cb63c4f0b4be3f267f8200d23e1aaae
Alin Hîrtopanu <alinhirtopanu@gmail.com>
2022-05-09 09:07:08 +0300

    removed body error

--- FrontEnd/concept-descriptor/src/components/Results/ResponseComponent.js
+++ FrontEnd/concept-descriptor/src/components/Results/ResponseComponent.js
@@ -84,7 +84,7 @@ class ResponseComponent extends React.Component {
         // asa faci call: resultObject.concep1.def
     
         return (
-            <body>
+            <div>
             <div className='Body-div'>
                 <div className='different'>
                     <div className='left-div'>
@@ -145,7 +145,7 @@ class ResponseComponent extends React.Component {
             <footer className='footer'>
 
             </footer>
-        </body>      
+        </div>      
         )
         
     }
