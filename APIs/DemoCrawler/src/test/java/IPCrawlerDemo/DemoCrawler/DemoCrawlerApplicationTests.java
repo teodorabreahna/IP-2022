4022164f7287980bfecae432dbcd1f12aa8532ec
Paaless <goth929@gmail.com>
2022-05-15 14:15:41 +0300

    Moved unit testing from folder UnitTest to folder test/IPCrawlerDemo.DemoCrawler

--- APIs/DemoCrawler/src/test/java/IPCrawlerDemo/DemoCrawler/DemoCrawlerApplicationTests.java
+++ APIs/DemoCrawler/src/test/java/IPCrawlerDemo/DemoCrawler/DemoCrawlerApplicationTests.java
@@ -1,11 +1,39 @@
 package IPCrawlerDemo.DemoCrawler;
 
+import IPCrawlerDemo.DemoCrawler.models.CrawlerInputObject;
+import IPCrawlerDemo.DemoCrawler.services.CrawlerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DemoCrawlerApplicationTests {
 
+	CrawlerService crawler;
+	CrawlerInputObject input;
+	@BeforeEach
+	void setUp() {
+		crawler = new CrawlerService();
+		input = new CrawlerInputObject();
+		input.setConcept1("car");
+		input.setConcept2("man");
+	}
+	@Test
+	@DisplayName("No data found/Wrong input")
+	void testWrongInput() throws IOException {
+		assertNotNull(crawler.processInfo(input));
+	}
+	@Test
+	@DisplayName("Has content")
+	void testContent(){
+		assertEquals(1000,crawler.processInfo(input).size());
+	}
 	@Test
 	void contextLoads() {
 	}
