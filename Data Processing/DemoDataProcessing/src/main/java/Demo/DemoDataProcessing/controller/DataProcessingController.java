dc35423814cc199a951f49fd0361011cec3ad992
Tudor Gălățan <tudorgalatan@gmail.com>
2022-05-17 19:13:03 +0300

    Updates MonkeyLearn

--- Data Processing/DemoDataProcessing/src/main/java/Demo/DemoDataProcessing/controller/DataProcessingController.java
+++ Data Processing/DemoDataProcessing/src/main/java/Demo/DemoDataProcessing/controller/DataProcessingController.java
@@ -1,10 +1,14 @@
 package Demo.DemoDataProcessing.controller;
 
 
+import com.monkeylearn.MonkeyLearnException;
 import org.springframework.web.bind.annotation.*;
 import Demo.DemoDataProcessing.models.DataProcessingInputObject;
 import Demo.DemoDataProcessing.models.DataProcessingOutputObject;
 import Demo.DemoDataProcessing.Services.DataProcessingService;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.List;
 
 @CrossOrigin
@@ -15,8 +19,7 @@ public class DataProcessingController {
 
     @PostMapping("/dataprocessing")
     @ResponseBody
-    public List<DataProcessingOutputObject> execute(@RequestBody List<DataProcessingInputObject> dataProcessingInputObject)
-    {
+    public List<DataProcessingOutputObject> execute(@RequestBody List<DataProcessingInputObject> dataProcessingInputObject) throws MonkeyLearnException, IOException {
         // http://localhost:8090/crawl?concept=hello&concept2=world
 
         return dataProcessingService.processInfo(dataProcessingInputObject);
