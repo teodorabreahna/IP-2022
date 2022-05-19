3a7abccc9f3019f5ddd1461e5c96d29e0073ccfc
alupoaiemircea <alupoaiemircea@gmail.com>
2022-05-19 11:58:29 +0300

    FilterLanguages updated so you can actually filter by an english dictionary

--- APIs/DemoCrawler/src/main/java/IPCrawlerDemo/DemoCrawler/backend/twittercollect/FilterLanguages.java
+++ APIs/DemoCrawler/src/main/java/IPCrawlerDemo/DemoCrawler/backend/twittercollect/FilterLanguages.java
@@ -12,10 +12,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
 public class FilterLanguages {
-        static List<CrawlerOutputObject> tweetObjects = null;
+    static List<CrawlerOutputObject> tweetObjects = null;
 
     public static List<CrawlerOutputObject> readJSON() {
         try {
@@ -28,37 +33,44 @@ public class FilterLanguages {
         return tweetObjects;
     }
 
-    public static void filterJSON(){
+    public static void filterJSON() {
         System.out.println("Reading the JSON file, waiting to filter.");
         readJSON();
-        for(CrawlerOutputObject s: tweetObjects){
-            String copy = s.getText();
-            byte[] copyBytes = copy.getBytes();
-            String asciiEncodedString = new String(copyBytes, StandardCharsets.US_ASCII);
-            char questionmark = '�';
-            Integer freq = 0;
-            for(int i = 0; i<asciiEncodedString.length(); i++){
-                if(questionmark == asciiEncodedString.charAt(i))
-                    ++freq;
+        try {
+            Set<String> dictionary = new HashSet<>(Files.readAllLines(Paths.get("dictionary.txt")));
+            //System.out.println(dictionary);
+            for (CrawlerOutputObject s : tweetObjects) {
+                String copy = s.getText();
+                String[] split = copy.split(" |,|\\.");
+                int freq = 0;
+                for (String word : split) {
+                    word = word.toLowerCase(Locale.ROOT);
+                    if (!dictionary.contains(word)) {
+                        //System.out.println(word + " NOT in dictionary");
+                        freq++;
+                    }
+
+                    if (freq > s.getText().length()/2) {
+                        tweetObjects.remove(s);
+                    }
+                }
             }
-            if(freq>(asciiEncodedString.length())/2){
-                tweetObjects.remove(s);
-            }
+        } catch (IOException e) {
+            System.out.println("dictionary not found or sth.");
+            e.printStackTrace();
         }
         System.out.println("Filtering JSON object.");
         ObjectMapper tweetMapper = new ObjectMapper();
-        try{
+        try {
             System.out.println("Writing to filtered JSON file.");
             tweetMapper.writeValue(new File("output_twitter.json"), tweetObjects);
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
-
-//    public static void main(String args[]){
-//        filterJSON();
-//    }
+    public static void main(String args[]){
+        filterJSON();
+    }
 
 }
