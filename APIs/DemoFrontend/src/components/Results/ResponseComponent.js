603c689cf92ff862583a153fa7f8404f27708d65
Bogdanf21 <79158552+Bogdanf21@users.noreply.github.com>
2022-05-21 21:34:09 +0300

    error handling + error page + loading screen

--- APIs/DemoFrontend/src/components/Results/ResponseComponent.js
+++ APIs/DemoFrontend/src/components/Results/ResponseComponent.js
@@ -68,37 +68,37 @@ class ResponseComponent extends React.Component {
         this.resultObject = this.state.jsonData:
 
         this.resultObject = new ResultObject(
-            {
+            { 
                 "concept1": "mere",
                 "def1": [
-                    "A body of standing water, such as a lake or a pond. More specifically, it can refer to a lake that is broad in relation to its depth. Also included in place names such as Windermere."
+                 "A body of standing water, such as a lake or a pond. More specifically, it can refer to a lake that is broad in relation to its depth. Also included in place names such as Windermere."
                 ],
-
+    
                 "chart1": "chart?bkg-white&c={ type: 'bar', data: { labels: ['ana', 'mere', 'are', 'mere'], datasets: [{ label: 'Users', data: [1, 1, 1, 1, 1] }] }}",
                 "concept2": "pere",
                 "def2": [
-                    "A priest of the Roman Catholic Church, especially a French one. Also used as a title preceding the name of such a priest.",
-                    "Sr. - Used after a proper name that is common to a father and his son to indicate that the father is being referred to rather than the son (junior, fils)."
+                "A priest of the Roman Catholic Church, especially a French one. Also used as a title preceding the name of such a priest.",
+                "Sr. - Used after a proper name that is common to a father and his son to indicate that the father is being referred to rather than the son (junior, fils)."
                 ],
-
+    
                 "chart2": "chart?c={ type: 'bar', data: { labels: ['ana', 'pere', 'are', 'ana', 'pere'], datasets: [{ label: 'Users', data: [1, 1, 1, 1, 1] }] }}",
-                "intersect": [
-                    "ana",
-                    "are"
+                "intersect" : [
+                "ana",
+                "are"
                 ],
-
+    
                 "diferit1": [
-                    "mere"
+                "mere"
                 ],
-
-                "diferit2": [
-                    "pere"
+    
+                "diferit2":[
+                "pere"
                 ],
-
-                "chartPie": "chart?c={type: 'pie', data: {labels: ['mere', 'pere'], datasets: [{ data:[3,3]}]}}",
+    
+                "chartPie": "chart?c={type: 'pie', data:{labels: ['mere', 'pere'], datasets: [{ data:[3,3]}]}}",
                 "chartBar": "chart?c={type: 'bar', data: {labels: ['Nouns', 'Adjectives', 'Verbs'], datasets:[{label: 'mere', data:[2,0,1]},{label:'pere', data:[2,0,1]}]}}"
-
-            }
+    
+                }
         );
 
        
@@ -110,99 +110,98 @@ class ResponseComponent extends React.Component {
         if (this.state.hasError)
             return <ErrorModal jsonData = {this.state.jsonData} />
         else
-            return (
-                <div>
-                    <div className='Body-div'>
-                        <div className='different'>
-                            <div className='left-div'>
-
-                                <div className='text-info'>
-                                    <h2>First concept:</h2>
-                                </div>
-
-                                <div className='Concept'>
-                                    {this.props.secondConcept};
-                                </div>
-                                <div className='text-info'>
-                                    <h2>First concept definition:</h2>
-                                </div>
-                                <div className='info'>
-                                    {this.resultObject.def1};
-                                </div>
-                                <div className='text-info'>
-                                    <h2>First concept chart:</h2>
-                                </div>
-                                <div className='chart'>
-                                    <img src={`https://quickchart.io/${this.resultObject.chart1}`} alt="Chart Missing" />
-                                </div>
-
-                            </div>
-
-                            <div className='right-div'>
-                                <div className='text-info'>
-                                    <h2>Second concept:</h2>
-                                </div>
-                                <div className='Concept'>
-                                    {this.resultObject.concept2}
-                                </div>
-                                <div className='text-info'>
-                                    <h2>Second concept definition:</h2>
-                                </div>
-                                <div className='info'>
-                                    {this.resultObject.def2}
-                                </div>
-
-                                <div className='text-info'>
-                                    <h2>Second concept chart:</h2>
-                                </div>
-
-                                <div className='chart'>
-                                    <img src={`https://quickchart.io/${this.resultObject.chart2}`} alt="Chart Missing" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className='text-info'>
-                            <h2>Intersected:</h2>
-                        </div>
-                        <div className='common'>
-                            {this.resultObject.intersect}
-                        </div>
+            return (<div>
+            <div className='Body-div'>
+                <div className='different'>
+                    <div className='left-div'>
 
                         <div className='text-info'>
-                            <h2>Diferit1:</h2>
-                        </div>
-                        <div className='common'>
-                            {this.resultObject.diferit1}
+                            <h2>First concept:</h2>
                         </div>
 
+                        <div className='Concept'>
+                            {this.props.secondConcept};
+                        </div>
                         <div className='text-info'>
-                            <h2>Diferit2:</h2>
+                             <h2>First concept definition:</h2>
+                         </div>
+                        <div className='info'>
+                            {this.resultObject.def1};
                         </div>
-                        <div className='common'>
-                            {this.resultObject.diferit2}
-                        </div>
+                        <div className='text-info'>
+                             <h2>First concept chart:</h2>
+                         </div>
+                        
+                        <img className='chart' src = {`https://quickchart.io/${this.resultObject.chart1}`} alt = "Chart Missing"/>
+                        
 
-                        <div className='text-info'>
-                            <h2>ChartPie:</h2>
-                        </div>
-                        <div className='common'>
-                            <img src={`https://quickchart.io/${this.resultObject.chartPie}`} alt="Chart Missing" />
-                        </div>
-
-                        <div className='text-info'>
-                            <h2>ChartBar:</h2>
-                        </div>
-                        <div className='common'>
-                            <img src={`https://quickchart.io/${this.resultObject.chartBar}`} alt="Chart Missing" />
-                        </div>
-                        <br />
-                        <br />
                     </div>
 
-                    <footer className='footer'>
-                    </footer>
+                    <div className='right-div'>
+                        <div className='text-info'>
+                            <h2>Second concept:</h2>
+                        </div>
+                        <div className='Concept'>
+                            {this.resultObject.concept2}
+                        </div>
+                        <div className='text-info'>
+                             <h2>Second concept definition:</h2>
+                         </div>
+                        <div className='info'>
+                            {this.resultObject.def2}
+                        </div>
+
+                        <div className='text-info'>
+                             <h2>Second concept chart:</h2>
+                         </div>
+
+                       
+                        <img className='chart' src = {`https://quickchart.io/${this.resultObject.chart2}`} alt = "Chart Missing"/>
+                        
+                    </div>
                 </div>
-            )
+                <div className='text-info'>
+                    <h2>Intersected:</h2>
+                </div>
+                <div className='common'>
+                    {this.resultObject.intersect}
+                </div>
+
+                <div className='text-info'>
+                    <h2>Diferit1:</h2>
+                </div>
+                <div className='common'>
+                    {this.resultObject.diferit1}
+                </div>
+
+                <div className='text-info'>
+                    <h2>Diferit2:</h2>
+                </div>
+                <div className='common'>
+                    {this.resultObject.diferit2}
+                </div>
+
+                <div className='text-info'>
+                    <h2>ChartPie:</h2>
+                </div>
+                
+                <img className='chart2' src = {`https://quickchart.io/${this.resultObject.chartPie}`} alt = "Chart Missing"/>
+                
+
+                <div className='text-info'>
+                    <h2>ChartBar:</h2>
+                </div>
+                
+                <img className='chart2' src = {`https://quickchart.io/${this.resultObject.chartBar}`} alt = "Chart Missing"/>
+               
+                <br />
+                <br />
+            </div> 
+
+            <footer className='footer'>
+            </footer>
+        </div>      
+        )
 
     }
 
