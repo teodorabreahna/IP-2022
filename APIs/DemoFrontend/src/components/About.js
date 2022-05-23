f81535c908963bdf26017fa9dc293f4db89dc8c8
Burciu Daniel <103900660+dburciu@users.noreply.github.com>
2022-05-23 09:09:05 +0300

    Update About.js

--- APIs/DemoFrontend/src/components/About.js
+++ APIs/DemoFrontend/src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function About() {
     return (
-       <section class="section1"> 
+       <section class="section2"> 
         <div class="im-pr-about">
 
             <div class="info-about">
