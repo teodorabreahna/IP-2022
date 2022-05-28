4635aa6ba16810e6be928bb0ece7e1b8845cc3a9
Burciu Daniel <103900660+dburciu@users.noreply.github.com>
2022-05-28 16:30:51 +0300

    Update About.js

--- APIs/DemoFrontend/src/components/About.js
+++ APIs/DemoFrontend/src/components/About.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function About() {
     return (
-       <section class="section2"> 
-        <div class="im-pr-about">
+       <section classname="section2"> 
+        <div className="im-pr-about">
 
-            <div class="info-about">
+            <div className="info-about">
                 <h2>Our information is gathered from<br /> various platforms such as Reddit,<br /> Twitter and so on.</h2>
                 <h3>Enter one or two concepts, and we will<br />give you the information you need.<br /> <br /> It is that easy!</h3>
             </div>
